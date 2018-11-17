c8473726cb7f4c92b2f5ea75e920733ee3d7ff24
pow6 <fake.existences@gmail.com>
2018-11-17 23:30:42 +0900

    提出フォルダ追加ンゴ

--- kadai7/DFT1-7addIFFT.c
+++ kadai7/DFT1-7addIFFT.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #define PI M_PI
 struct complex{
     double re;
@@ -9,6 +10,8 @@ struct complex{
 };
 void intro(void);
 void FFT(struct complex *xn,struct complex *XK,int N);
+void IFFT(struct complex *xn,struct complex *XK,int N);
+void DFT(int N,int k,int a,struct complex *xn,struct complex *XK);
 struct complex calcSum(struct complex cm1,struct complex cm2);
 struct complex calcDif(struct complex cm1,struct complex cm2);
 struct complex calcPro(struct complex cm1,struct complex cm2);
@@ -25,14 +28,14 @@ int countBit(int value);
 
 int main()
 {
+    clock_t c1,c2;
     struct complex *xn;
     struct complex *XK;
     double *data,*dftData;
     int mode,n,k,N;
-    int i,win;
-    int a,beta;
+    int i,a;
     FILE *fpIn,*fpOUT;
-    char fileName[24];
+    char fileName[24],fileNameOut[24];
     intro();
     printf("File Name to Read >>");
     scanf("%s",fileName);
@@ -42,14 +45,14 @@ int main()
         exit(1);
     }
     printf("File Name to Write >>");
-    scanf("%s",fileName);
-    fpOUT=fopen(fileName,"w");
+    scanf("%s",fileNameOut);
+    fpOUT=fopen(fileNameOut,"w");
     if(fpOUT==NULL){
-        printf("cannot open file[%s]",fileName);
+        printf("cannot open file[%s]",fileNameOut);
         exit(1);
     }
     do{
-        printf("Mode DFT:1 IDFT:2 >>");
+        printf("Mode FFT:1 DFT:2 >>");
         scanf("%d",&mode);
         getchar();
     }while(mode!=1&&mode!=2);
@@ -63,14 +66,39 @@ int main()
         XK[i].re=0;
         XK[i].im=0;
     }
-    for(i=0;i<N;i++){       
-        fscanf(fpIn,"%lf",&xn[i].re);
+    if(mode==1){
+        //FFT
+        for(i=0;i<N;i++){       
+            fscanf(fpIn,"%lf",&xn[i].re);
+        }
+        c1=clock();
+        FFT(xn,XK,N);
+        c2=clock();
+        for(i=0;i<N;i++){
+            printf("X%d=%lf+(%lfj)\n",i,XK[i].re,XK[i].im);
+            fprintf(fpOUT,"%lf %lf\n",XK[i].re,XK[i].im);
+            //fprintf(fpOUT,"%d %lf %lf %lf %lf\n",i,XK[i].re,XK[i].im,sqrt(XK[i].re*XK[i].re+XK[i].im*XK[i].im),atan2(XK[i].im,XK[i].re));
+        }
+    }else{
+        //DFT
+        for(i=0;i<N;i++){       
+            fscanf(fpIn,"%lf",&xn[i].re);
+        }
+        c1=clock();
+        for(i=0;i<N;i++){
+            DFT(N,i,a,xn,XK);
+        }
+        c2=clock();
+        for(i=0;i<N;i++){
+            printf("X%d=%lf+(%lfj)\n",i,XK[i].re,XK[i].im);
+            fprintf(fpOUT,"%lf %lf\n",XK[i].re,XK[i].im);
+            //fprintf(fpOUT,"%d %lf %lf %lf %lf\n",i,XK[i].re,XK[i].im,sqrt(XK[i].re*XK[i].re+XK[i].im*XK[i].im),atan2(XK[i].im,XK[i].re));
+        }
     }
-    FFT(xn,XK,N);
-    for(i=0;i<N;i++){
-        printf("X%d=%lf+(%lfj)\n",i,XK[i].re,XK[i].im);
-    }
-   fclose(fpIn);
+    printf("処理にかかった時間：%f",(double)(c2-c1)/CLOCKS_PER_SEC);
+    fclose(fpIn);
+    fclose(fpOUT);
+
 }
 
 
@@ -84,38 +112,16 @@ void FFT(struct complex *xn,struct complex *XK,int N)
     int numOfBit=countBit(numOfArray);  //段数（2^rのr段）
     int numOfCalc=numOfArray/2;         //一段あたりのバタフライ演算数
     struct complex *wnk,tmp;                //回転子のポインタ
-wnk=(struct complex*)malloc(sizeof(struct complex)*N);
-twid(wnk,numOfArray);               //回転子の作成
-
-printf("回転行列\n");
-for(i=0;i<N;i++){
-printf("%lf   ",wnk[i].re);
-printf("%lf\n",wnk[i].im);
-}
-printf("読み取った行列\n");
-for(i=0;i<N;i++){
-printf("%lf   ",xn[i].re);
-printf("%lf\n",xn[i].im);
-}
+    wnk=(struct complex*)malloc(sizeof(struct complex)*N);
+    twid(wnk,numOfArray);               //回転子の作成
     arrayReverse(xn,N);                   //この時点でxnはビット逆順に並ぶ
-printf("回転後");
-for(i=0;i<N;i++){
-printf("%lf   ",xn[i].re);
-printf("%lf\n",xn[i].im);
-}
     now=numOfArray/2;
     for(i=0,shift=1;i<numOfBit;i++){            //段数分の繰り返し処理
         for(j=0;j<shift;j++){       //1段あたりのバタフライ演算数分の繰り返し処理ここから
             for(z=0;z<now;z++){       //ここまでで1段分
-            //printf("******\n");
-            //printf("[%d]=[%d]+[%d]*[%d]\n",now+z,j,j+shift,((i*shift)%((numOfArray-1))*j)%(numOfCalc));
-            //printf("[%d]=[%d]-[%d]*[%d]\n",now+z+shift,j,j+shift,((i*shift)%((numOfArray-1))*j)%(numOfCalc));
                 tmp=calcPro(xn[shift*2*z+j+shift],wnk[j*now]);
                 xn[shift*2*z+j+shift]=calcDif(xn[shift*2*z+j],tmp);      //上段計算
                 xn[shift*2*z+j]=calcSum(xn[shift*2*z+j],tmp);    //下段計算
-            //    XK[now+z]=calcSum(xn[j],calcPro(xn[j+shift],wnk[(((i*shift)%(numOfArray-1))*j)%(numOfCalc)]));      //上段計算
-            //    XK[now+shift+z]=calcSum(xn[j],calcPro(xn[j+shift],wnk[(((i*shift)%numOfArray-1)*j)%numOfCalc]));    //下段計算
-                //XK[now+shift+z]=calcSum(xn[j],xn[j+shift]*wnk[(((i*shift)%7)*j)%4]);    //下段計算
             }
         }
         now=now>>1;
@@ -126,12 +132,60 @@ printf("%lf\n",xn[i].im);
     }
 }
 //IFFTを実行する(FFTをnで割る処理を追加する)
+void IFFT(struct complex *xn,struct complex *XK,int N)
+{
+    int i,j,z;
+    int numOfArray=sizeof(xn)/sizeof(struct complex);
+    numOfArray=N;
+    int shift,now,next;                        //計算時バタフライでずらす量
+    int numOfBit=countBit(numOfArray);  //段数（2^rのr段）
+    int numOfCalc=numOfArray/2;         //一段あたりのバタフライ演算数
+    struct complex *wnk,tmp;                //回転子のポインタ
+    wnk=(struct complex*)malloc(sizeof(struct complex)*N);
+    twid(wnk,numOfArray);               //回転子の作成
+    for(i=0;i<numOfArray;i++){
+        wnk[i]=calcCJG(wnk[i]);
+    }
+    now=numOfArray/2;
+    for(i=0,shift=1;i<numOfBit;i++){            //段数分の繰り返し処理
+        for(j=0;j<shift;j++){       //1段あたりのバタフライ演算数分の繰り返し処理ここから
+            for(z=0;z<now;z++){       //ここまでで1段分
+                tmp=calcPro(xn[shift*2*z+j+shift],wnk[j*now]);
+                xn[shift*2*z+j+shift]=calcDif(xn[shift*2*z+j],tmp);      //上段計算
+                xn[shift*2*z+j]=calcSum(xn[shift*2*z+j],tmp);    //下段計算
+            }
+        }
+        now=now>>1;
+        shift=shift<<1;                 //1段進むごとにバタフライ演算でずらす量は２倍になる
+    }
+    for(i=0;i<numOfArray;i++){
+        XK[i].re=xn[i].re/N;
+        XK[i].im=xn[i].im/N;
+    }
+}
 
+//DFTを実行する
+void DFT(int N,int k,int a,struct complex *xn,struct complex *XK)
+{
+    int n;
+    XK[k].re=0;
+    XK[k].im=0;
+    for(n=0;n<N;n++){
+        XK[k].re+=xn[n].re*cos(2*PI*n*k/N)+a*xn[n].im*sin(2*PI*n*k/N);
+        XK[k].im+=xn[n].im*cos(2*PI*n*k/N)-a*xn[n].re*sin(2*PI*n*k/N);
+    }
+}
 
 void intro(void)
 {
     printf("2018年 課題5 4J02 池口恭司\n");
     printf("使い方：\n");
+    printf("**************************\n");
+    printf("1.『File Name to Read >>』読み込むデータのファイル名を入力する\n");
+    printf("2.『File Name to Write >>』書き込むデータのファイル名を入力する\n");
+    printf("3.『Mode FFT:1 IFFT:2』実行したい関数を選択する。FFTは１，IFFTは２を入力\n");
+    printf("4.『Amount of Data』点数（N）を入力する\n");
+    printf("5.指定したデータがファイルとコマンドラインに出力される\n");
     printf("**************************\n");
 }
 
@@ -204,19 +258,12 @@ void arrayReverse(struct complex *bitArray,int N)
         bitOrder[i]=0;
     }
     bitReverse_makeOrder(eltValue,bitOrder);
-for(i=0;i<N;i++){
-printf("bitOrder:%d bitArray:%d %d\n",bitOrder[i],bitArray[i].re,bitArray[i].im);
-}
     for(i=0;i<eltValue;i++){
         tmpArray[bitOrder[i]]=bitArray[i];
     }
     for(i=0;i<eltValue;i++){
         bitArray[i]=tmpArray[i];
     }
-for(i=0;i<N;i++){
-printf("order:%d bitArray:%d %d\n",i,bitArray[i].re,bitArray[i].im);
-}
-
 }
 
 //ビット逆順行列を作成する
